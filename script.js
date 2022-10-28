5038c3b968d407be764a8cc219a228cabe6d89e9
Vinicius Victor <vine.victordev@gmail.com>
2022-10-28 00:37:41 -0300

    Função Cadastro de Produtos

---
+++ script.js
@@ -0,0 +1,17 @@
+// eCommerce
+var id = [], nameProduct = [], price = [], review = [];
+var idNumber = 1
+var arrCounter = 0
+
+Registration();
+
+function Registration() {
+    id[arrCounter] = idNumber;
+    nameProduct[arrCounter] = prompt("Nome do produto:");
+    price[arrCounter] = parseInt(prompt("Preço do produto:"));
+    review[arrCounter] = parseInt(prompt("Avaliação 1-5:"));
+    idNumber++;
+    arrCounter++;
+    return id, nameProduct, price, review;
+};
+
