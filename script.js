1d9ae5f2c6732f7b2049e065f474e494fb5f8a69
Vinicius Victor <vine.victordev@gmail.com>
2022-10-28 01:04:38 -0300

    Função Busca por Nome do Produto

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var id = [], nameProduct = [], price = [], review = [];
 var idNumber = 1
 var arrCounter = 0
-var search
 
-search = parseInt(prompt("Digite a ID do produto."))
-SearchById(search);
+
+Registration();
+SearchByName();
 
 
 function Registration() {
@@ -18,11 +18,23 @@ function Registration() {
     return id, nameProduct, price, review;
 };
 
-function SearchById(search) {
+function SearchById() {
+    var search = parseInt(prompt("Digite o ID do Produto."))
     for (var i = 0; i < id.length; i++) {
         if (search == id[i]) {
             console.log(id[i] + " | " + nameProduct[i] + " | " + price[i] + " | " + review[i]);
         };
     }
 }
+
+function SearchByName() {
+    var search = prompt("Digite o Nome do Produto.");
+    for (var i = 0; i < id.length; i++) {
+        if (search == nameProduct[i]) {
+            console.log(nameProduct[i] + " | " + " ID = " + id[i]);
+        }
+    }
+}
+
+
 
