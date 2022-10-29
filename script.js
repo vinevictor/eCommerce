dfb1778066a7455091d37520c3e8311deb286a4b
Vinicius Victor <vine.victordev@gmail.com>
2022-10-28 22:40:11 -0300

    Função Ordenar por Preço

--- script.js
+++ script.js
@@ -3,18 +3,12 @@ var id = [], nameProduct = [], price = [], review = [];
 var idNumber = 1;
 var arrCounter = 0;
 var arrSupId = [], arrSupName = [], arrSupPrice = [], arrSupReview = [];
-var smallestNumber = 0
-
-
+var counterArraySup = 0
 
 id[0] = 2, id[1] = parseInt(30), id[2] = 3, id[3] = 1, id[4] = 4;
 nameProduct[0] = "mouse5", nameProduct[1] = "mouse4", nameProduct[2] = "mouse3", nameProduct[3] = "mouse2", nameProduct[4] = "mouse1";
-price[0] = 50, price[1] = 40, price[2] = 30, price[3] = 20, price[4] = 10;
+price[0] = parseInt(40), price[1] = parseInt(50), price[2] = parseInt(30), price[3] = parseInt(20), price[4] = parseInt(10);
 review[0] = 5, review[1] = 5, review[2] = 4, review[3] = 3, review[4] = 2;
-
-OrderById()
-
-
 
 function Registration() {
     id[arrCounter] = idNumber;
@@ -33,7 +27,7 @@ function SearchById(id) {
             console.log(id[i] + " | " + nameProduct[i] + " | " + price[i] + " | " + review[i]);
         };
     }
-}
+};
 
 function SearchByName(nameProduct) {
     var search = prompt("Digite o Nome do Produto.");
@@ -42,11 +36,16 @@ function SearchByName(nameProduct) {
             console.log(nameProduct[i] + " | " + " ID = " + id[i]);
         }
     }
-}
+};
 
 
 function OrderById() {
-    var counterId = id.length - 1;
+    arrSupId = [];
+    arrSupName = [];
+    arrSupPrice = [];
+    arrSupReview = [];
+    counterArraySup = id.length - 1;
+    var smallestNumber = 0;
     for (var i = 0; i < id.length; i++) {
         smallestNumber = id[i]
         for (var i2 = 0; i2 < id.length; i2++) {
@@ -56,13 +55,13 @@ function OrderById() {
         }
         for (var i3 = 0; i3 < id.length; i3++) {
             if (smallestNumber == id[i3]) {
-                arrSupId[counterId] = id[i3]
-                arrSupName[counterId] = nameProduct[i3]
-                arrSupPrice[counterId] = price[i3]
-                arrSupReview[counterId] = review[i3]
+                arrSupId[counterArraySup] = parseInt(id[i3])
+                arrSupName[counterArraySup] = nameProduct[i3]
+                arrSupPrice[counterArraySup] = parseInt(price[i3])
+                arrSupReview[counterArraySup] = review[i3]
 
                 id[i3] = 0
-                counterId--
+                counterArraySup--
             }
         }
     }
@@ -74,7 +73,48 @@ function OrderById() {
     price = arrSupPrice
     review = arrSupReview
 
-    return id, nameProduct, price, review;
-}
+
+};
+
+function OrderByPrice() {
+    arrSupId = [];
+    arrSupName = [];
+    arrSupPrice = [];
+    arrSupReview = [];
+    counterArraySup = 0;
+    var biggestPrice = 0;
+    for (var i = 0; i < price.length; i++) {
+        biggestPrice = price[i];
+        for (var i2 = 0; i2 < price.length; i2++) {
+            if (biggestPrice < price[i2]) {
+                biggestPrice = price[i2]
+            }
+        }
+        for (var i3 = 0; i3 < price.length; i3++) {
+            if (biggestPrice == price[i3]) {
+                arrSupId[counterArraySup] = parseInt(id[i3])
+                arrSupName[counterArraySup] = nameProduct[i3]
+                arrSupPrice[counterArraySup] = parseInt(price[i3])
+                arrSupReview[counterArraySup] = review[i3]
+                counterArraySup++
+                price[i3] = 0
+
+            }
+        }
+    }
+    for (var i = 0; i < arrSupId.length; i++) {
+        console.log(arrSupId[i] + " | " + arrSupName[i] + " | " + arrSupPrice[i] + " | " + arrSupReview[i]);
+    }
+    id = arrSupId;
+    nameProduct = arrSupName;
+    price = arrSupPrice;
+    review = arrSupReview;
+};
+
+
+OrderByPrice();
+
+
+
 
 
